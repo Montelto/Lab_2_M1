b55dccd64d0974ae78c0d5f1fdc9397a77ff2582
Montelto <mcfarland#@wisc.edu>
2020-02-07 14:44:45 -0600

    Mid Friday

--- app/src/main/java/com/example/lab2/MainActivity.java
+++ app/src/main/java/com/example/lab2/MainActivity.java
@@ -3,6 +3,10 @@ package com.example.lab2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void clickFunction(View view) {
+        EditText myTextField = (EditText) findViewById(R.id.myTextField);
+
+        Toast.makeText(MainActivity.this, myTextField.getText().toString(),
+                Toast.LENGTH_LONG).show();
     }
 }
